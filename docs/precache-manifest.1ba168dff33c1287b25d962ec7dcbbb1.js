2c4bb19da50f7727080f11ae4d324b7ee2e4595c
Sven Meinhardt <sven.meinhardt@posteo.de>
2020-11-27 21:04:47 +0000

    4. added docs

---
+++ docs/precache-manifest.1ba168dff33c1287b25d962ec7dcbbb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76eb0c3dd755b7b07e4fd3d2ba5e9cac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec95e0705d95dacff6cd",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1859dd6263464fe763d6",
+    "url": "/static/js/2.56cc79d6.chunk.js"
+  },
+  {
+    "revision": "ec95e0705d95dacff6cd",
+    "url": "/static/js/main.ed406ef5.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
